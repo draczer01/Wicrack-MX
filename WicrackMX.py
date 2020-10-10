3f8a9d0c0c297f27cb8977fb8ec510de8eea9bd0
Carlos Antonio Gutierrez Rodriguez <carlosgtzrdz.94@gmail.com>
2020-10-10 02:12:19 -0500

    Select interface handler and menu refresh validation

--- WicrackMX.py
+++ WicrackMX.py
@@ -33,8 +33,15 @@ target_BSSID =""
 interface_mac=""
 list2 = []
 
-for x in interfaces:
-  list2.append({ 'title': x, 'type': INTSEL, 'command': x })
+def refresh_options_list2():
+  global list2
+  global interfaces
+  interfaces = netifaces.interfaces()
+  list2 = []
+  for x in interfaces:
+    list2.append({ 'title': x, 'type': INTSEL, 'command': x })
+
+refresh_options_list2()
 
 #crates an empty menu data
 menu_data = {}
@@ -59,11 +66,12 @@ def getObjectives(interface):
 
 def set_data():
     global menu_data
+    global list2
     menu_data = {
       'title': "Wicrack MX", 'type': MENU, 'subtitle': "Selected interface: " + selected_interface + " Interface mode: " + interface_mode + " Target: " + target, 'options': [
         { 'title': "Select network interface", 'type': MENU, 'subtitle': "Selected interface: " + selected_interface, 'options': list2 },
         { 'title': "Put interface in monitor mode", 'type': MAINCOMMAND, 'command': 'sudo airmon-ng start ' + selected_interface, 'status': 'monitor' },
-        { 'title': "Put interface in managed mode", 'type': MAINCOMMAND, 'command': 'sudo airmon-ng stop ' + selected_interface + 'mon', 'status': 'managed' },
+        { 'title': "Put interface in managed mode", 'type': MAINCOMMAND, 'command': 'sudo airmon-ng stop ' + selected_interface, 'status': 'managed' },
         { 'title': "Fix network issues", 'type': MAINCOMMAND, 'command': 'sudo airmon-ng check kill \n sudo service NetworkManager restart', 'status': 'null' },
         { 'title': "Select Wifi target", 'type': WIFISEL, 'subtitle': "selected target: " + target, 'options': wifilist },
         { 'title': "DOS attack menu", 'type': MENU, 'subtitle': "DOS attak menu", 'options': [
@@ -158,6 +166,8 @@ def processmenu(menu, parent = None):
   global wilist
   global target_BSSID
   global interface_mac
+  global list2
+  global interfaces
   wifilist = []
   optioncount = len(menu['options'])
   exitmenu = False
@@ -195,9 +205,15 @@ def processmenu(menu, parent = None):
       curses.curs_set(1)         # reset doesn't do this right
       curses.curs_set(0)
       #os.system('echo > log ' + str(menu))
-
+      if menu['options'][getin]['status'] == 'monitor' and selected_interface[-3:] != 'mon':
+        selected_interface = selected_interface + 'mon'
+      elif menu['options'][getin]['status'] == 'managed' and selected_interface[-3:] == 'mon':
+        selected_interface = selected_interface[0:-3]
+        os.system('sudo ifconfig ' + selected_interface + ' up')
 
     elif menu['options'][getin]['type'] == MENU:
+      refresh_options_list2()
+      set_data()
       screen.clear() #clears previous screen on key press and updates display based on pos
       menu = menu_data
       processmenu(menu['options'][getin], menu) # display the submenu
@@ -231,6 +247,7 @@ def processmenu(menu, parent = None):
       screen.refresh()
     
     elif menu['options'][getin]['type'] == INTSEL:
+      #errorMessage({"status": 200, "description": "Hello World"}, screen, curses)
       selected_interface = menu['options'][getin]['command']
       is_monitor = selected_interface[-3:]
       if is_monitor == 'mon':
@@ -246,16 +263,15 @@ def processmenu(menu, parent = None):
 
     elif menu['options'][getin]['type'] == EXITMENU:
       exitmenu = True
+
 # Custom errors
-def errorMessage(errorCode):
- switcher = {
-     1: "",
-     2: "",
-     3: "",
-     4: "",
-     5: ""
- }
- print switcher.get(errorType, "Unrecognizable error")
+def errorMessage(errorCode, clearScreen, pressKey):
+  clearScreen.clear()
+  clearScreen.refresh()
+  print('')
+  print(str(errorCode["status"]) + ': ' + errorCode["description"] + ', press any key to continue...')
+  pressKey.initscr().getch()
+
 # Main program
 processmenu(menu_data)
 curses.endwin() #VITAL! This closes out the menu system and returns you to the bash prompt.
