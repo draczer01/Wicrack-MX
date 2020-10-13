caa52d86f1f5d1355cc50204dec73d40f58e7a2e
Humberto <draczer01@gmail.com>
2020-10-13 05:41:18 -0500

    added dictionary modules

---
+++ dictionary_modules/axtel.py
@@ -0,0 +1,21 @@
+import itertools
+print("Bienvenido al generador de contrasenias de Axtel, fabor de ingresar los ultimos 4 caracteres del ssid");
+
+par1 = raw_input("AXTEL-EXTREMO")
+
+print(par1);
+
+print("por fabor eliga el nombre del archivo para generar las contrasenias");
+
+
+filename = raw_input();
+
+archivo = open(filename,'w');
+
+gen = itertools.combinations_with_replacement('0123456789QWERTYUIOPASDFGHJKLZXCVBNM',4) 
+for password in gen:                                                       
+    par2 = password[0]+password[1]+password[2]+password[3] + par1
+    with open(filename, 'a') as file:
+        file.write(par2 + '\n')
+    
+   # print(par2)
